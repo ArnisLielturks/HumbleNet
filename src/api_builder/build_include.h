07f0461a2f3ca99fc87fb4eeecf1f4b53ffe1f06
Arnis Lielturks <arnis.lielturks@gmail.com>
2020-05-27 21:48:15 +0300

    even more cleanup

--- src/api_builder/build_include.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-void buildInclude(const std::string& mode, const std::string& input, const std::string& output);
--- src/api_builder/build_loader.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-void buildLoader(const std::string& input, const std::string& output);
